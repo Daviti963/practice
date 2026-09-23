a128640e6f867c16322dc3251bea7eff30c09226
Daviti963 <datagvaradze13@gmail.com>
2026-09-23 22:42:26 +0400

    blabla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,14 @@ export default function App() {
   return (
     <main className={styles.main}>
       <h1>he</h1>
+      <h2>hello</h2> <h1>he</h1>
+      <h2>hello</h2> <h1>he</h1>
+      <h2>hello</h2> <h1>he</h1>
+      <h2>hello</h2> <h1>he</h1>
+      <h2>hello</h2> <h1>he</h1>
+      <h2>hello</h2> <h1>he</h1>
       <h2>hello</h2>
-      <h3>1</h3>
-      <h4></h4>
-      <h5></h5>
+    
     </main>
   );
 }
